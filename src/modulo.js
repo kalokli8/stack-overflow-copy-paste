a178ddb0cf6fa8172606e25014568c04db320e6b
Peter Li <42929320+kalokli8@users.noreply.github.com>
2022-04-10 13:36:08 +0800

    Add files via upload

---
+++ src/modulo.js
@@ -0,0 +1,13 @@
+/* eslint-disable semi */
+export default modulo;
+
+/**
+ * This method will get modulo (remainder) from two numbers
+ *
+ * @param {Number} num1 - first number
+ * @param {Number} num2 - second number
+ * @return {Number} - Result of modulo of first number divivied by second number
+ */
+function modulo(num1, num2) {
+  return num1 % num2;
+}
